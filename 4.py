a98faf794882ad7a4489340d187d0bef27805cd3
elblopa <56318113+elblopa@users.noreply.github.com>
2019-10-08 17:14:23 -0300

    problema 4

---
+++ 4.py
@@ -0,0 +1,7 @@
+
+letra = str(input("Ingrese una letra: "))
+
+if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
+    print("Su letra es una vocal")
+else:
+    print("Su letra es una consonante")
