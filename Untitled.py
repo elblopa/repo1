f2ab3e71c811dc49fb5cf3c42bcffa6915fec245
elblopa <56318113+elblopa@users.noreply.github.com>
2019-10-08 16:05:59 -0300

    adding problem number one, Moodle

---
+++ Untitled.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+H = int(input("Digite la hora: "))
+M = int(input("Digite el minuto: "))
+S = int(input("Digite el segundo: "))
+
+hora = H*3600 + M*60 + S
+
+print("Los segundos son: ", hora)
+
+
+# In[3]:
+
+
+2*3600 + 30*60 + 20
+
