481c5cd193108aebf8892db1cc840e46ddccaf81
elblopa <56318113+elblopa@users.noreply.github.com>
2019-10-08 16:59:34 -0300

    Problema 2,3 Moodle

---
+++ xd.py
@@ -0,0 +1,16 @@
+a = input("Digite un numero: ")
+b = input("Digite otro: ")
+c = input("Digite otro: ")
+
+if (a>=b>=c):
+    print(c, b, a)
+elif (a>c>b):
+    print(b, c, a)
+elif (b>a>c):
+    print(c, a, b)
+elif (b>c>a):
+    print(a, c, b)
+elif (c>a>b):
+    print(b, a, c)
+elif (c>b>a):
+    print(a, b, c)
